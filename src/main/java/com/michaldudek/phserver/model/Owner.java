f241f0c0af0123a2e2a7dc21dc2b7945ab996148
Michal Dudek <dudek.michal1996@gmail.com>
2020-02-19 00:05:13 +0100

    AllArgsConstructors added to model classes

--- src/main/java/com/michaldudek/phserver/model/Owner.java
+++ src/main/java/com/michaldudek/phserver/model/Owner.java
@@ -1,5 +1,6 @@
 package com.michaldudek.phserver.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Owner {
 
     private String username;
--- src/main/java/com/michaldudek/phserver/model/User.java
+++ src/main/java/com/michaldudek/phserver/model/User.java
@@ -1,5 +1,6 @@
 package com.michaldudek.phserver.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import java.util.Date;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Document
 public class User {
 
