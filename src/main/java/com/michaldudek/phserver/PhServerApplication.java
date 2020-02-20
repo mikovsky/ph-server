383e7031782b64ea1cc353927ad23127724c1ca2
Michal Dudek <dudek.michal1996@gmail.com>
2020-02-20 23:15:36 +0100

    CORS allowed methods update

--- src/main/java/com/michaldudek/phserver/PhServerApplication.java
+++ src/main/java/com/michaldudek/phserver/PhServerApplication.java
@@ -2,7 +2,10 @@ package com.michaldudek.phserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,6 +13,19 @@ public class PhServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PhServerApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "DELETE");
+            }
+        };
     }
 
 }
