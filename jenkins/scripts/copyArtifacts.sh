f59e54afa8870a099eb26bfd0e7124d8474b1a75
Michal Dudek <dudek.michal1996@gmail.com>
2020-02-27 22:57:47 +0100

    Jenkinsfile sudo added

--- jenkins/scripts/copyArtifacts.sh
+++ jenkins/scripts/copyArtifacts.sh
@@ -1,13 +1,1 @@
 #!/bin/bash
-
-echo 'Setting NAME environment variable...'
-set -x
-NAME=$(mvn help:evaluate -Dexpression=project.name | grep "^[^\[]")
-set +x
-
-echo 'Setting VERSION environment variable...'
-set -x
-VERSION=$(mvn help:evaluate -Dexpression=project.version | grep "^[^\[]")
-set +x
-
-sudo cp "/target/${NAME}-${VERSION}.jar" "/mnt/zonedisk1/daily_artifacts"
